2ce83a51bd2dabdba9cb87c6bec8a3a8064bc3a0
alexghiondea <ghiondea.alexandru@microsoft.com>
2019-10-04 15:57:28 -0700

    Remove and sort usings.

--- src/Client.cs
+++ src/Client.cs
@@ -1,10 +1,8 @@
 ﻿using Azure;
 using Azure.Core.Pipeline;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using ZenHub.Pipeline;
