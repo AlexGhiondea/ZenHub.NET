339896dd2b3ca890b7438c9d45891c479d758d59
Alex Ghiondea <AlexGhiondea@users.noreply.github.com>
2019-10-05 20:57:35 -0700

    Make the build system on Mac work like on Windows (#6)

--- build.sh
+++ build.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+if [ "$#" -eq 0 ]
+then
+    _config=Debug
+else
+    _config=$1
+fi
 
+echo --------------------------
+echo !!! Restoring packages !!!
+echo --------------------------
 dotnet restore
 
-dotnet build
+echo -------------------------
+echo !!! Cleaning solution !!!
+echo -------------------------
+dotnet clean -c $_config
 
-dotnet test --no-build tests/ZenHub.Tests.csproj
+echo -------------------------
+echo !!! Building solution !!!
+echo -------------------------
+dotnet build -c $_config
+
+echo ---------------------
+echo !!! Running tests !!!
+echo ---------------------
+dotnet test --no-build -c $_config tests/ZenHub.Tests.csproj
+
+echo ------------------------------
+echo !!! Creating NuGet package !!!
+echo ------------------------------
+dotnet pack -c $_config
+
+echo on
